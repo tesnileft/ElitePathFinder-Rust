7f79228a70d70d9cff5112809a8399b17e997ed5
Tesni <tesnileft@gmail.com>
2026-07-19 00:49:34 +0200

    Basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
-use gtk::{Application, ApplicationWindow, Button, ListBox, glib};
+use glib::clone;
+use gtk::{Application, ApplicationWindow, Button, ListBox, TextView, gio, glib};
 use gtk::{Entry, prelude::*};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 mod parser;
 
+const APP_ID: &str = "tesnileft.ElitePathfinder-Rust";
+
 fn main() -> glib::ExitCode {
-    let application = Application::builder()
-        .application_id("com.example.FirstGtkApp")
-        .build();
+    gio::resources_register_include!("epf-r.gresource").expect("Failed to register resources.");
+    let application = Application::builder().application_id(APP_ID).build();
 
     application.connect_activate(build_ui);
 
+    let current_log = File::open("/mnt/gamestorage/SteamLibrary/steamapps/compatdata/359320/pfx/drive_c/users/steamuser/AppData/Local/Frontier Developments/Elite Dangerous/Journal12543831.cache").unwrap();
+    let mut current_log_buf = BufReader::new(current_log);
+
+    thread::spawn(move || {
+        loop {
+            let mut new_contents = String::new();
+            current_log_buf.read_to_string(&mut new_contents).unwrap();
+        }
+    });
+
     application.run()
 }
+
+fn read_newest_log_file() -> Result<String, std::io::Error> {
+    let current_log_result = File::open(
+        "/mnt/gamestorage/SteamLibrary/steamapps/compatdata/359320/pfx/drive_c/users/steamuser/AppData/Local/Frontier Developments/Elite Dangerous/Journal12543831.cache",
+    );
+    match current_log_result {
+        Ok(file) => {
+            let current_log: File = file;
+            let mut current_log_buf = BufReader::new(current_log);
+            let mut new_contents = String::new();
+            current_log_buf.read_to_string(&mut new_contents).unwrap();
+            Ok(new_contents)
+        }
+        Err(error) => Err(error),
+    }
+}
+
 fn build_ui(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("First GTK Program")
+        .title("Elite Pathfinder")
         .default_width(350)
         .default_height(70)
         .build();
@@ -24,15 +58,74 @@ fn build_ui(app: &Application) {
     let filepathfield = Entry::builder().build();
 
     vertical_column1.append(&filepathfield);
-    let button = Button::builder()
+    let copybutton = Button::builder()
         .label("Button (cool)")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
-    button.connect_clicked(move |button| button.set_label(&filepathfield.buffer().text()));
+    copybutton.connect_clicked(move |button| button.set_label(&filepathfield.buffer().text()));
+    vertical_column1.append(&copybutton);
+
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Create channel that can hold at most 1 message at a time
+    let (sender, receiver) = async_channel::bounded(1);
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |_| {
+        let sender = sender.clone();
+        // The long running operation runs now in a separate thread
+        gio::spawn_blocking(move || {
+            // Deactivate the button until the operation is done
+            sender
+                .send_blocking(false)
+                .expect("The channel needs to be open.");
+
+            let five_seconds = Duration::from_secs(5);
+            thread::sleep(five_seconds);
+            // Activate the button again
+            sender
+                .send_blocking(true)
+                .expect("The channel needs to be open.");
+        });
+    });
+
+    let textbox = TextView::builder().editable(false).can_focus(false).build();
+    textbox.buffer().set_text("Sample Text");
+    vertical_column1.append(&textbox);
+    let readbutton = Button::builder()
+        .label("Read Latest Log")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    readbutton.connect_clicked(move |_| match read_newest_log_file() {
+        Ok(filecontents) => textbox.buffer().set_text(&filecontents),
+        Err(err) => textbox.buffer().set_text(&err.to_string()),
+    });
+    vertical_column1.append(&readbutton);
+
+    // The main loop executes the asynchronous block
+    glib::spawn_future_local(clone!(
+        #[weak]
+        button,
+        async move {
+            while let Ok(enable_button) = receiver.recv().await {
+                button.set_sensitive(enable_button);
+            }
+        }
+    ));
+
     vertical_column1.append(&button);
+
     window.set_child(Some(&vertical_column1));
     window.present();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,150 @@
+use serde::Deserialize;
+use serde_json::{Result, Value};
 use std::fs;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::time;
+use std::vec;
+struct LogFile {
+    timestamp: time::SystemTime,
+    event: String,
+}
+#[derive(Deserialize)]
+enum SignalType {
+    FleetCarrier,
+}
+#[derive(Deserialize)]
+enum GameMode {
+    Solo,
+    Group,
+    Open,
+}
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct FSSSignal {
+    SignalName: String,
+    SignalType: SignalType,
+    IsStation: bool,
+}
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct FSDTarget {
+    Name: String,
+    SystemAddress: u32,
+    StarClass: String,
+    RemainingJumpsInRoute: u32,
+}
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct Shiplocker {
+    Items: Vec<Item>,
+}
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct Item {
+    Name: String,
+    Name_Localised: String,
+    OwnerId: u32,
+    Count: u32,
+}
 
-fn parselogfile(path: String) {
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct RawMaterial {
+    Name: String,
+    Count: u32,
+}
+
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct LocalisedMaterial {
+    Name: String,
+    Name_Localised: String,
+    Count: u32,
+}
+
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct Materials {
+    Raw: Vec<RawMaterial>,
+    Manufactured: Vec<LocalisedMaterial>,
+    Encoded: Vec<LocalisedMaterial>,
+}
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct LoadGame {
+    FID: String,
+    Commander: String,
+    Horizons: bool,
+    Odyssey: bool,
+    Ship: String,
+    ShipLocalised: String,
+    ShipId: u32,
+    ShipName: String,
+    ShipIdent: String,
+    FuelLevel: f32,
+    FielCapacity: f32,
+    GameMode: GameMode,
+    Group: String,
+    Credits: u32,
+    Loan: u32,
+    language: String,
+    gameversion: String,
+    build: String,
+}
+
+fn parselogfile(path: String) -> Result<()> {
     let log = fs::read_to_string(path).expect("Log read succesfully");
     let loglines = log.lines();
-    for line in loglines {}
+    for line in loglines {
+        parselogline(line);
+    }
+    Ok(())
+}
+
+fn parselogline(line: &str) -> Result<()> {
+    let v: Value = serde_json::from_str(line)?;
+    let event_type = v["event"].as_str().unwrap();
+    println!("{}", event_type);
+    match event_type {
+        "LoadGame" => {
+            let journal_entry: LoadGame = serde_json::from_str(line)?;
+        }
+        "Cargo" => {}
+        "Friends" => {}
+        "Commander" => {}
+        "Materials" => {}
+        "Rank" => {}
+        "Progress" => {}
+        "Reputation" => {}
+        "EngineerProgress" => {}
+        "SquadronStartup" => {}
+        "Statistics" => {}
+        "Location" => {}
+        "Powerplay" => {}
+        "Music" => {}
+        "Shiplocker" => {}
+        "Missions" => {}
+        "Loadout" => {}
+        "MissionAbandoned" => {}
+        "NavRoute" => {}
+        "FSDTarget" => {}
+        "CommunityGoalJoin" => {}
+        "CommunityGoal" => {}
+        "Undocked" => {}
+        "StartJump" => {}
+        "ReceiveText" => {}
+        "NavRouteClear" => {}
+        "FSSSignalDiscovered" => {
+            let journal_entry: FSSSignal = serde_json::from_str(line)?;
+        }
+        "FSDJump" => {}
+        "SupercruiseDestinationDrop" => {}
+        "SupercruiseExit" => {}
+        "Scan" => {}
+        other => {
+            println!("Undocumented event type: {other}")
+        }
+    }
+    Ok(())
 }
